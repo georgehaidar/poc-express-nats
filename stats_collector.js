09ddc8b537f6c33160200658adf7b4870e9105b6
Georges Haidar <georges.haidar@myob.com>
2016-07-28 18:19:16 +1000

    added stats collector microservice

--- stats_collector.js
+++ stats_collector.js
@@ -1,5 +1,5 @@
 const nats = require('nats').connect({'servers': ['nats://nats:4222']})
 
-const sid = nats.subscribe('stats.*', {'queue':'stats'}, function(req, reply, subject) {
+const sid = nats.subscribe('stats.>', {'queue':'stats'}, function(req, reply, subject) {
   console.log(`[${subject}] - ${req}`)
 })
